0b7e50766c0bd102f3424ede7643b6f40d445ab5
Sintari <aleks.bernaciak@gmail.com>
2016-10-10 03:32:44 +0200

    code refactor, prepare for more then 2 players

--- cards.js
+++ cards.js
@@ -59,212 +59,173 @@ var cards = [
     },{
         Suit: "Hearts",
         Number: 2,
-        drawn: false,
         Img: 'assets/img/2_of_hearts.png'
     }, {
         Suit: "Hearts",
         Number: 3,
-        drawn: false,
         Img: 'assets/img/3_of_hearts.png'
     }, {
         Suit: "Hearts",
         Number: 4,
-        drawn: false,
         Img: 'assets/img/4_of_hearts.png'
     }, {
         Suit: "Hearts",
         Number: 5,
-        drawn: false,
         Img: 'assets/img/5_of_hearts.png'
     }, {
         Suit: "Hearts",
         Number: 6,
-        drawn: false,
         Img: 'assets/img/6_of_hearts.png'
     }, 
-    // {
-    //     Suit: "Hearts",
-    //     Number: 7,
-    //     drawn: false,
-    //     Img: 'assets/img/7_of_hearts.png'
-    // }, {
-    //     Suit: "Hearts",
-    //     Number: 8,
-    //     drawn: false,
-    //     Img: 'assets/img/8_of_hearts.png'
-    // }, {
-    //     Suit: "Hearts",
-    //     Number: 9,
-    //     drawn: false,
-    //     Img: 'assets/img/9_of_hearts.png'
-    // }, {
-    //     Suit: "Hearts",
-    //     Number: 10,
-    //     drawn: false,
-    //     Img: 'assets/img/10_of_hearts.png'
-    // }, {
-    //     Suit: "Hearts",
-    //     Number: 11,
-    //     Name: 'Jack',
-    //     drawn: false,
-    //     Img: 'assets/img/jack_of_hearts.png'
-    // }, {
-    //     Suit: "Hearts",
-    //     Number: 12,
-    //     Name: 'Queen',
-    //     drawn: false,
-    //     Img: 'assets/img/queen_of_hearts.png'
-    // },
-    //  {
-    //     Suit: "Hearts",
-    //     Number: 13,
-    //     Name: 'King',
-    //     drawn: false,
-    //     Img: 'assets/img/king_of_hearts.png'
-    // }, {
-    //     Suit: "Hearts",
-    //     Number: 14,
-    //     Name: 'Ace',
-    //     drawn: false,
-    //     Img: 'assets/img/ace_of_hearts.png'
-    // }, {
-    //     Suit: "Diamonds",
-    //     Number: 2,
-    //     drawn: false,
-    //     Img: 'assets/img/2_of_diamonds.png'
-    // }, {
-    //     Suit: "Diamonds",
-    //     Number: 3,
-    //     drawn: false,
-    //     Img: 'assets/img/3_of_diamonds.png'
-    // }, {
-    //     Suit: "Diamonds",
-    //     Number: 4,
-    //     drawn: false,
-    //     Img: 'assets/img/4_of_diamonds.png'
-    // }, {
-    //     Suit: "Diamonds",
-    //     Number: 5,
-    //     drawn: false,
-    //     Img: 'assets/img/5_of_diamonds.png'
-    // },
-    //  {
-    //     Suit: "Diamonds",
-    //     Number: 6,
-    //     drawn: false,
-    //     Img: 'assets/img/6_of_diamonds.png'
-    // }, {
-    //     Suit: "Diamonds",
-    //     Number: 7,
-    //     drawn: false,
-    //     Img: 'assets/img/7_of_diamonds.png'
-    // }, {
-    //     Suit: "Diamonds",
-    //     Number: 8,
-    //     drawn: false,
-    //     Img: 'assets/img/8_of_diamonds.png'
-    // }, {
-    //     Suit: "Diamonds",
-    //     Number: 9,
-    //     drawn: false,
-    //     Img: 'assets/img/9_of_diamonds.png'
-    // }, {
-    //     Suit: "Diamonds",
-    //     Number: 10,
-    //     drawn: false,
-    //     Img: 'assets/img/10_of_diamonds.png'
-    // }, {
-    //     Suit: "Diamonds",
-    //     Number: 11,
-    //     Name: 'Jack',
-    //     drawn: false,
-    //     Img: 'assets/img/jack_of_diamonds.png'
-    // }, {
-    //     Suit: "Diamonds",
-    //     Number: 12,
-    //     Name: 'Queen',
-    //     drawn: false,
-    //     Img: 'assets/img/queen_of_diamonds.png'
-    // }, {
-    //     Suit: "Diamonds",
-    //     Number: 13,
-    //     Name: 'King',
-    //     drawn: false,
-    //     Img: 'assets/img/king_of_diamonds.png'
-    // }, {
-    //     Suit: "Diamonds",
-    //     Number: 14,
-    //     Name: 'Ace',
-    //     drawn: false,
-    //     Img: 'assets/img/ace_of_diamonds.png'
-    // }, {
-    //     Suit: "Clubs",
-    //     Number: 2,
-    //     drawn: false,
-    //     Img: 'assets/img/2_of_clubs.png'
-    // }, {
-    //     Suit: "Clubs",
-    //     Number: 3,
-    //     drawn: false,
-    //     Img: 'assets/img/3_of_clubs.png'
-    // }, {
-    //     Suit: "Clubs",
-    //     Number: 4,
-    //     drawn: false,
-    //     Img: 'assets/img/4_of_clubs.png'
-    // }, {
-    //     Suit: "Clubs",
-    //     Number: 5,
-    //     drawn: false,
-    //     Img: 'assets/img/5_of_clubs.png'
-    // }, {
-    //     Suit: "Clubs",
-    //     Number: 6,
-    //     drawn: false,
-    //     Img: 'assets/img/6_of_clubs.png'
-    // }, {
-    //     Suit: "Clubs",
-    //     Number: 7,
-    //     drawn: false,
-    //     Img: 'assets/img/7_of_clubs.png'
-    // }, {
-    //     Suit: "Clubs",
-    //     Number: 8,
-    //     drawn: false,
-    //     Img: 'assets/img/8_of_clubs.png'
-    // }, {
-    //     Suit: "Clubs",
-    //     Number: 9,
-    //     drawn: false,
-    //     Img: 'assets/img/9_of_clubs.png'
-    // }, {
-    //     Suit: "Clubs",
-    //     Number: 10,
-    //     drawn: false,
-    //     Img: 'assets/img/10_of_clubs.png'
-    // }, {
-    //     Suit: "Clubs",
-    //     Number: 11,
-    //     Name: 'Jack',
-    //     drawn: false,
-    //     Img: 'assets/img/jack_of_clubs.png'
-    // }, {
-    //     Suit: "Clubs",
-    //     Number: 12,
-    //     Name: 'Queen',
-    //     drawn: false,
-    //     Img: 'assets/img/queen_of_clubs.png'
-    // }, {
-    //     Suit: "Clubs",
-    //     Number: 13,
-    //     Name: 'King',
-    //     drawn: false,
-    //     Img: 'assets/img/king_of_clubs.png'
-    // }, {
-    //     Suit: "Clubs",
-    //     Number: 14,
-    //     Name: 'Ace',
-    //     drawn: false,
-    //     Img: 'assets/img/ace_of_clubs.png'
-    // }
+    {
+        Suit: "Hearts",
+        Number: 7,
+        Img: 'assets/img/7_of_hearts.png'
+    }, {
+        Suit: "Hearts",
+        Number: 8,
+        Img: 'assets/img/8_of_hearts.png'
+    }, {
+        Suit: "Hearts",
+        Number: 9,
+        Img: 'assets/img/9_of_hearts.png'
+    }, {
+        Suit: "Hearts",
+        Number: 10,
+        Img: 'assets/img/10_of_hearts.png'
+    }, {
+        Suit: "Hearts",
+        Number: 11,
+        Name: 'Jack',
+        Img: 'assets/img/jack_of_hearts.png'
+    }, {
+        Suit: "Hearts",
+        Number: 12,
+        Name: 'Queen',
+        Img: 'assets/img/queen_of_hearts.png'
+    },
+     {
+        Suit: "Hearts",
+        Number: 13,
+        Name: 'King',
+        Img: 'assets/img/king_of_hearts.png'
+    }, {
+        Suit: "Hearts",
+        Number: 14,
+        Name: 'Ace',
+        Img: 'assets/img/ace_of_hearts.png'
+    }, {
+        Suit: "Diamonds",
+        Number: 2,
+        Img: 'assets/img/2_of_diamonds.png'
+    }, {
+        Suit: "Diamonds",
+        Number: 3,
+        Img: 'assets/img/3_of_diamonds.png'
+    }, {
+        Suit: "Diamonds",
+        Number: 4,
+        Img: 'assets/img/4_of_diamonds.png'
+    }, {
+        Suit: "Diamonds",
+        Number: 5,
+        Img: 'assets/img/5_of_diamonds.png'
+    },
+     {
+        Suit: "Diamonds",
+        Number: 6,
+        Img: 'assets/img/6_of_diamonds.png'
+    }, {
+        Suit: "Diamonds",
+        Number: 7,
+        Img: 'assets/img/7_of_diamonds.png'
+    }, {
+        Suit: "Diamonds",
+        Number: 8,
+        Img: 'assets/img/8_of_diamonds.png'
+    }, {
+        Suit: "Diamonds",
+        Number: 9,
+        Img: 'assets/img/9_of_diamonds.png'
+    }, {
+        Suit: "Diamonds",
+        Number: 10,
+        Img: 'assets/img/10_of_diamonds.png'
+    }, {
+        Suit: "Diamonds",
+        Number: 11,
+        Name: 'Jack',
+        Img: 'assets/img/jack_of_diamonds.png'
+    }, {
+        Suit: "Diamonds",
+        Number: 12,
+        Name: 'Queen',
+        Img: 'assets/img/queen_of_diamonds.png'
+    }, {
+        Suit: "Diamonds",
+        Number: 13,
+        Name: 'King',
+        Img: 'assets/img/king_of_diamonds.png'
+    }, {
+        Suit: "Diamonds",
+        Number: 14,
+        Name: 'Ace',
+        Img: 'assets/img/ace_of_diamonds.png'
+    }, {
+        Suit: "Clubs",
+        Number: 2,
+        Img: 'assets/img/2_of_clubs.png'
+    }, {
+        Suit: "Clubs",
+        Number: 3,
+        Img: 'assets/img/3_of_clubs.png'
+    }, {
+        Suit: "Clubs",
+        Number: 4,
+        Img: 'assets/img/4_of_clubs.png'
+    }, {
+        Suit: "Clubs",
+        Number: 5,
+        Img: 'assets/img/5_of_clubs.png'
+    }, {
+        Suit: "Clubs",
+        Number: 6,
+        Img: 'assets/img/6_of_clubs.png'
+    }, {
+        Suit: "Clubs",
+        Number: 7,
+        Img: 'assets/img/7_of_clubs.png'
+    }, {
+        Suit: "Clubs",
+        Number: 8,
+        Img: 'assets/img/8_of_clubs.png'
+    }, {
+        Suit: "Clubs",
+        Number: 9,
+        Img: 'assets/img/9_of_clubs.png'
+    }, {
+        Suit: "Clubs",
+        Number: 10,
+        Img: 'assets/img/10_of_clubs.png'
+    }, {
+        Suit: "Clubs",
+        Number: 11,
+        Name: 'Jack',
+        Img: 'assets/img/jack_of_clubs.png'
+    }, {
+        Suit: "Clubs",
+        Number: 12,
+        Name: 'Queen',
+        Img: 'assets/img/queen_of_clubs.png'
+    }, {
+        Suit: "Clubs",
+        Number: 13,
+        Name: 'King',
+        Img: 'assets/img/king_of_clubs.png'
+    }, {
+        Suit: "Clubs",
+        Number: 14,
+        Name: 'Ace',
+        Img: 'assets/img/ace_of_clubs.png'
+    }
 ]
--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 $( document ).ready(function() {
 	'use strict'
 
-	var p1Cards=[], p2Cards=[], p1CardsRaw=[], p2CardsRaw=[], tableCards=[];
+	var p1CardsRaw=[], p2CardsRaw=[], tableCards=[], pCards = [];
 	var sameCards1 = false, sameCards2 = false;
+	var playersCount = 2;
 
 	function drawnCard(card) {
 		var cardDetails = card.Number + ' ' + card.Suit;
@@ -13,40 +14,60 @@ $( document ).ready(function() {
 	}
 
 	function bindCard(bind, count) {
-		for (count; count >= 0; count--) {
-			var randCard = cards[Math.floor(Math.random() * cards.length)];
-			bind[i] =  randCard;
-			return drawnCard(bind[i]);
-		}
-	}
-
-	function showCard(dest, player, imgSrc) {
-		var imgDest = dest + ' + img';
-		$(imgDest).attr('src', imgSrc)
-		$(dest).text(player);
-	}
-
-	function checkFlush(cardsP1, cardsP2, cardsTable, cardsTableCount) {
-		var tableSuitCardsCount = [];
-
-		function checkFlushForPlayer(playerCards, playerNumber) {
-			var flushCount = 0;
-			for (var j = cardsTableCount - 1; j >= 0; j--) {
-
-				for (var i = 1; i >= 0; i--) {
-					if (playerCards[i].Suit == cardsTable[j].Suit) {
-
-						if (flushCount == 5) {
-							$('.figure-p' + playerNumber).text('Player ' + playerNumber + ' got ' + playerCards[i].Suit + ' flush.');
-						}
-						flushCount ++;
-						console.log('player' + playerNumber + ' ' + flushCount);
-					}
-				}
+		for (playersCount - 1; i >= 0; i--) {
+			for (count; count >= 0; count--) {
+				var randCard = cards[Math.floor(Math.random() * cards.length)];
+				bind[i] =  randCard;
+				return drawnCard(bind[i]);
 			}
 		}
-		checkFlushForPlayer(cardsP1, 1);
-		checkFlushForPlayer(cardsP2, 2);
+	}
+
+	function showCard(dest, imgSrc) {
+		var imgDest = dest + ' + img';
+		$(imgDest).attr('src', imgSrc)
+	}
+
+	function playerTableCards(playerCards, cardsTable, cardsTableCount, property) {
+		var array = [];
+		if (property == 'number') {
+			for (var i = 0; i < cardsTableCount; i++) {
+		    	array.push(cardsTable[i].Number);
+			}
+			for (var i = 0; i < playerCards.length; i++) {
+		    array.push(playerCards[i].Number);
+			}
+			console.log(array);
+			return array;
+		}
+		else if (property == 'suit') {
+			for (var i = 0; i < cardsTableCount; i++) {
+		    	array.push(cardsTable[i].Suit);
+			}
+			for (var i = 0; i < playerCards.length; i++) {
+		    array.push(playerCards[i].Suit);
+			}
+			return array;
+		}
+	}
+
+	function checkFlush(cardsPlayer, cardsTable, cardsTableCount, playerNumber) {
+		var arrSuit = playerTableCards(cardsPlayer, cardsTable, cardsTableCount, 'suit');
+
+		var frequency = {};  // array of frequency.
+		var max = 0;  // holds the max frequency.
+		var result;   // holds the max frequency element.
+		for(var v in arrSuit) {
+		        frequency[arrSuit[v]]=(frequency[arrSuit[v]] || 0)+1; // increment frequency.
+		        if(frequency[arrSuit[v]] > max) { // is this frequency > max so far ?
+		                max = frequency[arrSuit[v]];  // update max.
+		                result = arrSuit[v];          // update result.
+		                if (max >= 5) {
+		                	$('.figure-p' + playerNumber).text
+			    			('Player ' + playerNumber + ' got ' + arrSuit[v] + ' flush.');
+		                }
+		        }
+		}
 	}
 
 	function checkPairs(cardsP1, cardsP2, cardsTable, cardsTableCount) {
@@ -124,84 +145,66 @@ $( document ).ready(function() {
 		}
 	}
 
-	function checkStraight(cardsP1, cardsP2, cardsTable, cardsTableCount) {
+	function checkStraight(cardsPlayer, cardsTable, cardsTableCount, playerNumber) {
 
-		function cutArray(player) {
-			var arrNumbers = [];
-			for (var i = 0; i < cardsTableCount; i++) {
-		    	arrNumbers.push(cardsTable[i].Number);
-			}
-			for (var i = 0; i < player.length; i++) {
-		    arrNumbers.push(player[i].Number);
-			}
-			return arrNumbers;
-		}
+		var arrNumbers = playerTableCards(cardsPlayer, cardsTable, cardsTableCount, 'number');
+		var helperArray = {};
 
-		var arrNames1 = cutArray(cardsP1);
-		var arrNames2 = cutArray(cardsP2);
+		arrNumbers.forEach(function (e) { helperArray[e] = true; });
 
-		function checkStraightHelper(arr, playerNumber) {
-			var ao = {};
+		//now loop all in arrNumbers, and then loop again for 5
+		arrNumbers.forEach(function (num) {
+		  	var count = 0, l;
+		  	for (l = 0; l < 5; l ++) {
+		    	if (helperArray[num + l]) count ++;
+	 	 	}
 
-			arr.forEach(function (e) { ao[e] = true; });
+	  		if (count === 5) {
+		    //found, push into arrNumbers just to show nice in console
+		    	var nums = [];
+		    	for (l = 0; l < 5; l ++) {
+		      		nums.push(num + l);
+		    	}
 
-			//now loop all in arr, and then loop again for 5
-			arr.forEach(function (num) {
-			  	var count = 0, l;
-			  	for (l = 0; l < 5; l ++) {
-			    	if (ao[num + l]) count ++;
-		 	 	}
+		    	switch(nums[nums.length - 1]) {
+					case 14:
+					    $('.figure-p' + playerNumber).text
+			    		('Player ' + playerNumber + ' got straight from Ace to 10.');
+					    break;
 
-		  		if (count === 5) {
-			    //found, push into arr just to show nice in console
-			    	var nums = [];
-			    	for (l = 0; l < 5; l ++) {
-			      		nums.push(num + l);
-			    	}
+					case 13:
+					    $('.figure-p' + playerNumber).text
+			    		('Player ' + playerNumber + ' got straight from King to 9.');
+					    break;
 
-			    	switch(nums[nums.length - 1]) {
-						case 14:
-						    $('.figure-p' + playerNumber).text
-				    		('Player ' + playerNumber + ' got straight from Ace to 10.');
-						    break;
+					case 12:
+					    $('.figure-p' + playerNumber).text
+			    		('Player ' + playerNumber + ' got straight from Queen to 8.');
+					    break;
 
-						case 13:
-						    $('.figure-p' + playerNumber).text
-				    		('Player ' + playerNumber + ' got straight from King to 9.');
-						    break;
+					case 11:
+					    $('.figure-p' + playerNumber).text
+			    		('Player ' + playerNumber + ' got straight from Jack to 7.');
+					    break;
 
-						case 12:
-						    $('.figure-p' + playerNumber).text
-				    		('Player ' + playerNumber + ' got straight from Queen to 8.');
-						    break;
-
-						case 11:
-						    $('.figure-p' + playerNumber).text
-				    		('Player ' + playerNumber + ' got straight from Jack to 7.');
-						    break;
-
-						default:
-					    	$('.figure-p' + playerNumber).text
-				    		('Player ' + playerNumber + ' got straight from ' + nums[nums.length - 1] + ' to ' + nums[0] + '.');
-					}
-			  	}
-			})
-		}
-
-		checkStraightHelper(arrNames1, 1);
-		checkStraightHelper(arrNames2, 2);
+					default:
+				    	$('.figure-p' + playerNumber).text
+			    		('Player ' + playerNumber + ' got straight from ' + nums[nums.length - 1] + ' to ' + nums[0] + '.');
+				}
+		  	}
+		})
 	}
 
-	for (var i = 1; i >= 0; i--) {
-		p1Cards[i] = bindCard(p1CardsRaw, i+1);
-		p2Cards[i] = bindCard(p2CardsRaw, i+1);
+	for (var i = playersCount - 1; i >= 0; i--) {
+		bindCard(p1CardsRaw, i+1);
+		bindCard(p2CardsRaw, i+1);
 	}
 
 	for (var i = 4; i >= 0; i--) {
 		bindCard(tableCards, i+1);
 	}
 
-	for (var i = 1; i >= 0; i--) {
+	for (var i = playersCount - 1; i >= 0; i--) {
 		if (i != 0 && p1CardsRaw[i].Number == p1CardsRaw[i-1].Number) {
 			sameCards1 = true;
 			// alert('asd 1');
@@ -213,32 +216,44 @@ $( document ).ready(function() {
 	}
 
 	$('button#flop').click(function(){
-		showCard('.table-f1', 'Flop 1', tableCards[0].Img);
-		showCard('.table-f2', 'Flop 2', tableCards[1].Img);
-		showCard('.table-f3', 'Flop 3', tableCards[2].Img);
+		showCard('.table-f1', tableCards[0].Img);
+		showCard('.table-f2', tableCards[1].Img);
+		showCard('.table-f3', tableCards[2].Img);
 		checkPairs(p1CardsRaw, p2CardsRaw, tableCards, 3);
-		checkStraight(p1CardsRaw, p2CardsRaw, tableCards, 3);
-		checkFlush(p1CardsRaw, p2CardsRaw, tableCards, 3);
+
+		checkStraight(p1CardsRaw, tableCards, 3, 1);
+		checkStraight(p2CardsRaw, tableCards, 3, 2);
+
+		checkFlush(p1CardsRaw, tableCards, 3, 1);
+		checkFlush(p2CardsRaw, tableCards, 3, 2);
 
 		$('button#turn').removeClass('-disabled');
 		$(this).addClass("-disabled");
 	});
 
 	$('button#turn').click(function(){
-		showCard('.table-f4', 'Turn', tableCards[3].Img);
+		showCard('.table-f4', tableCards[3].Img);
 		checkPairs(p1CardsRaw, p2CardsRaw, tableCards, 4);
-		checkStraight(p1CardsRaw, p2CardsRaw, tableCards, 4);
-		checkFlush(p1CardsRaw, p2CardsRaw, tableCards, 4);
+
+		checkStraight(p1CardsRaw, tableCards, 4, 1);
+		checkStraight(p2CardsRaw, tableCards, 4, 2);
+
+		checkFlush(p1CardsRaw, tableCards, 4, 1);
+		checkFlush(p2CardsRaw, tableCards, 4, 2);
 		
 		$('button#river').removeClass('-disabled');
 		$(this).addClass("-disabled");
 	});
 
 	$('button#river').click(function(){
-		showCard('.table-f5', 'River', tableCards[4].Img);
+		showCard('.table-f5', tableCards[4].Img);
 		checkPairs(p1CardsRaw, p2CardsRaw, tableCards, 5);
-		checkStraight(p1CardsRaw, p2CardsRaw, tableCards, 5);
-		checkFlush(p1CardsRaw, p2CardsRaw, tableCards, 5);
+
+		checkStraight(p1CardsRaw, tableCards, 5, 1);
+		checkStraight(p2CardsRaw, tableCards, 5, 2);
+
+		checkFlush(p1CardsRaw, tableCards, 5, 1);
+		checkFlush(p2CardsRaw, tableCards, 5, 2);
 
 		$(this).addClass("-disabled");
 	});
@@ -252,9 +267,9 @@ $( document ).ready(function() {
 	// checkFlush(p1CardsRaw, p2CardsRaw, tableCards, 5);
 
 
-	showCard('.p1-c1', p1Cards[0], p1CardsRaw[0].Img);
-	showCard('.p1-c2', p1Cards[1], p1CardsRaw[1].Img);
-	showCard('.p2-c1', p2Cards[0], p2CardsRaw[0].Img);
-	showCard('.p2-c2', p2Cards[1], p2CardsRaw[1].Img);
+	showCard('.p1-c1', p1CardsRaw[0].Img);
+	showCard('.p1-c2', p1CardsRaw[1].Img);
+	showCard('.p2-c1', p2CardsRaw[0].Img);
+	showCard('.p2-c2', p2CardsRaw[1].Img);
 
 });
